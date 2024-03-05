191feabf1bb285fdeb502d550f0fd8efbc824d07
Ben Haddou Nordinne <nordinnebenhaddou@hotmail.com>
2024-03-05 19:25:57 +0100

    fin video 4 : boostrap

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash
+from models.my_form import MyForm
+import secrets
+
+secret_key = secrets.token_hex(16)
+
 
 # create flask instance
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = secret_key
 
 # create a route decorator
 
@@ -25,3 +30,16 @@ def page_not_found(error):
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template("500.html"),500
+
+@app.route('/name',methods=['GET','POST'])
+def name():
+        name=None
+        form=MyForm()
+        if form.validate_on_submit():
+            name=form.name.data
+            flash('Your name is '+ name)
+            form.name.data=''
+        return render_template('name.html',form=form,name=name)
+
+
+
